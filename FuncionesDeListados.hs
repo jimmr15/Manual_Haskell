6ba502e747647130afe384f486560cf62c0cdbb7
Jesus Ivan <jim15mr@gmail.com>
2016-11-06 01:03:43 -0800

    Video 9 - Funciones de Lista 2

---
+++ FuncionesDeListados.hs
@@ -0,0 +1,15 @@
+listadelista= [['a', 'b'],[ 'c', 'd']]
+lista = [1,2,3,4,5,6,7]
+ejercicio_8_3=reverse lista
+ejercicio_8_4=take 3 lista
+ejercicio_8_5=drop 2 lista
+lista2=[3,2,7,9,0]
+--minimum lista2
+--maximum lista2
+--maximum listaDelista
+ejercicio_8_10=sum lista
+ejercicio_8_11=1*2*3*4*5*6*7
+ejercicio_8_12=product lista
+ejercicio_8_13=8 `elem` lista
+ejercicio_8_14=4 `elem` lista
+ejercicio_8_15='c' `elem` listadelista !!1
