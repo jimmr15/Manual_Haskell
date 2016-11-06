9a68cc70504ba68532e13635694654c65d99dd31
Jesus Ivan <jim15mr@gmail.com>
2016-11-06 01:52:11 -0700

    Video 3 - Succ Min Max

---
+++ Succ_Min_Max.hs
@@ -0,0 +1,7 @@
+ejercicio_3_1=succ 9
+ejercicio_3_2=succ 'a'
+ejercicio_3_3=min 5 2
+ejercicio_3_4=max  12  78.3 
+ejercicio_3_5=max 4 (succ 7)
+ejercicio_3_6=succ (max  8  6)
+ejercicio_3_7=succ (max 8 (min  34.6  90))
