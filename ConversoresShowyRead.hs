51c635b4ae54c00564f6a8f085ac7ca585137767
Jesus Ivan <jim15mr@gmail.com>
2016-11-06 01:11:38 -0800

    Video 18 - Conversores Show y Read

---
+++ ConversoresShowyRead.hs
@@ -0,0 +1,8 @@
+ejercicio_18_1=show 3
+ejercicio_18_2=show 5.35346
+ejercicio_18_3=show False
+ejercicio_18_4=show [1,2,3]
+ejercicio_18_5=show (1,2,3)
+ejercicio_18_6=read "5.6" + 8.1
+ejercicio_18_7=read "False" && True
+ejercicio_18_8=read "[4,8,2,4]"++[9]
