784dbd6fe51cff848193827fa47cc2541e549d68
Jesus Ivan <jim15mr@gmail.com>
2016-11-06 01:05:22 -0800

    Video 11 - Funciones De Listas Infinitas

---
+++ FuncionesDeListasInfinitas.hs
@@ -0,0 +1,6 @@
+ejercicio_11_1= repeat 7
+ejercicio_11_2= cycle [1,2,6]
+ejercicio_11_3= replicate 5 60
+ejercicio_11_4=take 4 (repeat 3)
+ejercicio_11_5=take 4 (cycle "hola")
+ejercicio_11_6=take 4 (cycle "hola mundo")
