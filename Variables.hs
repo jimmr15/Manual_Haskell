f30a3005ec9a2de1dbebe0b2c94b19e3041de336
Jesus Ivan <jim15mr@gmail.com>
2016-11-06 01:50:20 -0700

    Video 2 - Variables

---
+++ Variables.hs
@@ -0,0 +1,26 @@
+ejercicio_2_1=5+3
+ejercicio_2_2=5/2
+ejercicio_2_3=5 `div` 2
+ejercicio_2_4=5`mod` 2
+ejercicio_2_5=5*9
+ejercicio_2_6=7-3
+ejercicio_2_7=7 - (2*2)
+ejercicio_2_8=True && False 
+ejercicio_2_9=True || False
+ejercicio_2_10= 5==6
+ejercicio_2_11= 7==7
+ejercicio_2_12=7/=3
+ejercicio_2_13=7/=7
+ejercicio_2_14=7>3
+ejercicio_2_15=7<7
+ejercicio_2_16= 7<=7
+ejercicio_2_17=7>=3
+ejercicio_2_18=7<=3
+ejercicio_2_19=7==(4+3)
+ejercicio_2_20=not True
+ejercicio_2_21=8+3.2
+ejercicio_2_22="Hola" ++ "Mundo"
+
+
+
+
